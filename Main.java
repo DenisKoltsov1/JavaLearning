0786fb61d5364c966d16d9875eda772a23297c0e
DenisKoltsov1 <44700055+DenisKoltsov1@users.noreply.github.com>
2025-03-20 15:22:53 +0300

    метод принимает два числа и возвр. произведение.
    Напишите метод, который принимает два числа и возвращает их произведение.

--- Main.java
+++ Main.java
@@ -1,30 +1,14 @@
+//Напишите метод, который принимает два числа и возвращает их произведение.
+
 public class Main {
     public static void main(String[] args) {
         IsFor f = new IsFor();
-        f.Massive();
+        System.out.println(f.toNumbers( 3, 4));
     }
 
     static class IsFor {
-        public void Massive() {
-            int[][] numbers = new int[3][3]; // Создаём двумерный массив 3x3
-            int value = 1; // Начальное значение для заполнения
-
-            // Заполнение массива числами от 1 до 9
-            for (int i = 0; i < numbers.length; i++) {
-                for (int j = 0; j < numbers[i].length; j++) {
-                    numbers[i][j] = value; // Присваиваем значение
-                    value++; // Увеличиваем значение
-                }
-            }
-
-            // Вывод массива
-            System.out.println("Двумерный массив 3x3:");
-            for (int i = 0; i < numbers.length; i++) {
-                for (int j = 0; j < numbers[i].length; j++) {
-                    System.out.print(numbers[i][j] + " "); // Выводим элементы строки
-                }
-                System.out.println(); // Переход на новую строку после каждой строки массива
-            }
+        public int toNumbers(int a, int b) {
+           return a*b;
         }
-    }
+        }
 }
