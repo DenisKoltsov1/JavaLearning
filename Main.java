6896ece3b4983b04e7f5dbbc51d9a58e0bfad990
DenisKoltsov1 <44700055+DenisKoltsov1@users.noreply.github.com>
2025-03-21 16:05:24 +0300

    геттеры и сеттеры.
    //Сделайте поля класса Car приватными, добавьте геттеры и сеттеры.

--- Main.java
+++ Main.java
@@ -1,17 +1,34 @@
-// Создайте класс Car с полями color и speed, выведите их для объекта.
-//Добавьте в класс Car метод drive(), который выводит "Машина едет"
+//Сделайте поля класса Car приватными, добавьте геттеры и сеттеры.
+
 public class Main {
     public static void main(String[] args) {
         Car mazda = new Car();
-        mazda.color = "Black";
-        mazda.speed = 245;
+        mazda.setColor("Black");  ;
+        mazda.setSpeed(256);
+
         mazda.drive();
-        System.out.println("Цвет  и  скорость: " + mazda.color + "  " + mazda.speed);
+        System.out.println("Цвет  и  скорость: " + mazda.getColor()+ "  " + mazda.getSpeed());
     }
 
     static class Car {
-        public String color;
-        public int speed;
+        private String color;
+        private int speed;
+
+        public void setColor(String color) {
+            this.color = color;
+        }
+
+        public void setSpeed(int speed) {
+            this.speed = speed;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+        public int getSpeed() {
+            return speed;
+        }
 
         public void drive()
         {
